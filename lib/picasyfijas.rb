3a882a2b4ce69fc35269c2a02c9670490f0b20e3
olman.docs <kleer@kleer-csd-box>
2017-06-30 11:52:58 -0500

    Se validan ahora sí las fijas

--- lib/picasyfijas.rb
+++ lib/picasyfijas.rb
@@ -2,40 +2,44 @@ class Picasyfijas
 
 	def initialize numeroIngresado="4021"
 
-	@numero = 1234#numero[1,2,3,4]
+	@numero = [1,2,3,4]
 
-	@numIngresado = numeroIngresado
+	@numIngresado = numeroIngresado.to_s
 	@numPicas = 0
 	@numFijas = 0
 	@numIntentos = 0
-
+	@retorno = Array.new
+	
 	end
 
 	def revisaPicasYFijas
-		if @numIngresado == @numero
-			return "El número ingresado es el correcto"
+		if @numIngresado == @numero.join
+			@retorno[0] = "El número ingresado es el correcto"
 		else
-			return "El número ingresado no es el correcto"
+			@retorno[0] = "El número ingresado no es el correcto"
 		end
-#		numDividido = numIngresado.split("")
+		#numDividido = @numIngresado.split("")
 #		if @numIntentos >= 4
 #			print "Se superaron el número de intentos"
 #		else
-#			for i in (0..@numero.length)
-#				for j in (0..numDividido.length)
-#					if i == j and numero[i] == numDividido[j]
-#						numFijas+=1 
-#						print @numero[i]
-#					elsif i != j and numero[i] == numDividido[j]
-#						numPicas+=1
-#					end
-#				end 
-#			end
+			for i in (0..@numero.length-1)
+				#for j in (0..numDividido.length)
+					puts "@numero[i]: #{@numero[i]} @numIngresado.slice(i): #{@numIngresado.slice(i)}" 
+					if @numero[i] == @numIngresado.slice(i).to_i
+						@numFijas+=1 
+						#print @numero[i]
+					#elsif i != j and numero[i] == numDividido[j]
+					#	numPicas+=1
+					end
+				#end 
+			end
 #			if @numFijas == 4
 #				print "Ganador"
 #			end
 #			@numIntentos+=1
 #		end
-#		print "Se encontraron "+numFijas+" fijas y "+numPicas+" picas"
+		@retorno[1] = "El número de fijas es #{@numFijas}"
+		return @retorno
+		#return 
 	end
 end
--- spec/picas_spec.rb
+++ spec/picas_spec.rb
@@ -4,12 +4,18 @@ describe Picasyfijas do
 
 	it "Debe validar que el número es igual" do
         pyf = Picasyfijas.new 1234
-        expect(pyf.revisaPicasYFijas).to eq "El número ingresado es el correcto"
+        expect(pyf.revisaPicasYFijas[0]).to eq "El número ingresado es el correcto"
     end
 
 	it "Debe validar que el número es diferente" do
         pyf = Picasyfijas.new 4021
-        expect(pyf.revisaPicasYFijas).to eq "El número ingresado no es el correcto"
+        expect(pyf.revisaPicasYFijas[1]).to eq "El número de fijas es 0"
     end
+
+    it "Debe validar el número de fijas" do
+        pyf = Picasyfijas.new 1754
+        expect(pyf.revisaPicasYFijas[1]).to eq "El número de fijas es 2"
+    end
+
 
 end
