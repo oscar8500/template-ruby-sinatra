c6691abb686de3fe1c395fb5a8150691c14e6b08
olman.docs <kleer@kleer-csd-box>
2017-06-30 14:20:18 -0500

    Validaciones Extra(Verde)

--- lib/picasyfijas.rb
+++ lib/picasyfijas.rb
@@ -1,38 +1,49 @@
 class Picasyfijas
 
 	def initialize numeroIngresado="4021"
-
-	@numero = [1,2,3,4]
-
-	@numIngresado = numeroIngresado.to_s
-	@numPicas = 0
-	@numFijas = 0
-	@numIntentos = 0
-	@retorno = Array.new
-	
+		@numero = [1,2,3,4]
+		@numIngresado = numeroIngresado.to_s
+		@numPicas = 0
+		@numFijas = 0
+		@numIntentos = 0
+		@retorno = Array.new
 	end
 
 	def revisaPicasYFijas
-		if @numIngresado == @numero.join
-			@retorno[0] = "El número ingresado es el correcto"
-		else
-			@retorno[0] = "El número ingresado no es el correcto"
-		end
-#		if @numIntentos >= 4
-#			print "Se superaron el número de intentos"
-#		else
-			for i in (0..@numero.length-1)
+		if(__validacionChars)
+			if(__validacionTamano)
+				for i in (0..@numero.length-1)
 					if @numero[i] == @numIngresado.slice(i).to_i
 						@numFijas+=1
 					elsif @numero.join.to_s.include?@numIngresado.slice(i)
 						@numPicas+=1
 					end
+				end
+				@retorno[1] = "El número de fijas es #{@numFijas}"
+				@retorno[2] = "El número de picas es #{@numPicas}"				
 			end
-#			@numIntentos+=1
-#		end
-		@retorno[1] = "El número de fijas es #{@numFijas}"
-		@retorno[2] = "El número de picas es #{@numPicas}"
+		end
 		return @retorno
-		#return 
+	end
+
+	def __validacionTamano
+		tamano = 4
+		if(tamano != @numIngresado.length)
+			@retorno[0] = "Número ingresado no es de 4 dígitos."
+			@retorno[1] = ""
+			@retorno[2] = ""
+			return false
+		end
+		return true
+	end
+
+	def __validacionChars
+		if !@numIngresado.scan(/\D/).empty?
+			@retorno[0] = "El valor ingresado no es numérico."
+			@retorno[1] = ""
+			@retorno[2] = ""
+			return false
+		end
+		return true
 	end
 end
--- spec/picas_spec.rb
+++ spec/picas_spec.rb
@@ -2,9 +2,14 @@ require './lib/picasyfijas'
 
 describe Picasyfijas do
 
-	it "Debe validar que el número es igual" do
-        pyf = Picasyfijas.new 1234
-        expect(pyf.revisaPicasYFijas[0]).to eq "El número ingresado es el correcto"
+    it "Debe validar que ingrese 4 dígitos exactos" do
+        pyf = Picasyfijas.new 754
+        expect(pyf.revisaPicasYFijas[0]).to eq "Número ingresado no es de 4 dígitos."
+    end
+
+    it "Debe validar que ingrese 4 dígitos exactos" do
+        pyf = Picasyfijas.new 7541892
+        expect(pyf.revisaPicasYFijas[0]).to eq "Número ingresado no es de 4 dígitos."
     end
 
 	it "Debe validar que el número es diferente" do
@@ -20,5 +25,10 @@ describe Picasyfijas do
     it "Debe validar el número de picas" do
         pyf = Picasyfijas.new 7541
         expect(pyf.revisaPicasYFijas[2]).to eq "El número de picas es 2"
+    end
+
+    it "Debe validar que las variables ingresadas son sólo números" do
+        pyf = Picasyfijas.new "754t"
+        expect(pyf.revisaPicasYFijas[0]).to eq "El valor ingresado no es numérico."
     end
 end
